3c529da86b8d8385b7d172606408d0fb7566b7d6
mohammedmukhtar05 <M.O.MUKHTAR-2022@hull.ac.uk>
2024-11-04 16:24:10 +0000

    Added these files from the repos I created to work on.

---
+++ Project/Project/Program.cs
@@ -0,0 +1,32 @@
+﻿
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Who are you?");
+        Console.WriteLine("1. Student");
+        Console.WriteLine("2. Personal Supervisor");
+        Console.WriteLine("3. Senior Tutor");
+
+        string input = Console.ReadLine();
+
+        switch (input)
+        {
+            case "1":
+                Student student = new Student();
+              
+                break;
+            case "2":
+                PersonalSupervisor supervisor = new PersonalSupervisor();
+               
+                break;
+            case "3":
+                SeniorTutor tutor = new SeniorTutor();
+              
+                break;
+            default:
+                Console.WriteLine("Invalid selection.");
+                break;
+        }
+    }
+}
